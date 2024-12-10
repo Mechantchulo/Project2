8c7754fb299283931ac22133c9ccc0cc0f77616e
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-10 12:39:02 +0300

    `Added custom login and signup forms with CSS class updates to users/forms.py`

--- users/forms.py
+++ users/forms.py
@@ -6,3 +6,21 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser 
         fields = ('username', 'email', 'password1', 'password2', 'role')
+
+
+# users/forms.py
+from django import forms
+from allauth.account.forms import LoginForm, SignupForm
+
+class CustomLoginForm(LoginForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['login'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password'].widget.attrs.update({'class': 'form-control'})
+
+class CustomSignupForm(SignupForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
