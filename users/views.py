53dbabd84d0b9e221593a0c4a708acd2c4296be8
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-12 01:16:06 +0300

    cteated a signup view function in users/views.py

--- users/views.py
+++ users/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login,authenticate
-from .forms import CustomUserCreationForm
+# from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 
-def register(request):
+# def register(request):
+#     if request.method == 'POST':
+#         form = CustomUserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('dashboard') # Redirect to dashboard after successful registration
+#     else:
+#         form = CustomUserCreationForm()
+#     return render(request, 'registration/register.html', {'form': form})
+
+def signup(request):
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('dashboard') # Redirect to dashboard after successful registration
+            return redirect('home')  # Adjust as necessary
     else:
-        form = CustomUserCreationForm()
-    return render(request, 'registration/register.html', {'form': form})
-
+        form = UserCreationForm()
+    return render(request, 'templates/account/signup.html', {'form': form})
 
 
 class CustomPasswordChangeView(PasswordChangeView):
