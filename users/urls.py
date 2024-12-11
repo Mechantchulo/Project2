e4cb42a3f1c59bd92a4fbafcb25122f50f4b2e7e
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-12 00:53:31 +0300

    Added paths for login, logout and sign up in users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import register, CustomPasswordChangeView
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('password/change/', CustomPasswordChangeView.as_view(), name='account_change_password'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
