05fc08419336066af84a0b3c2f252414b64c7b7b
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-12 01:30:52 +0300

    Remove redundant register functionality and consolidate with signup

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import register, CustomPasswordChangeView
+from .views import CustomPasswordChangeView
 from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
     path('password/change/', CustomPasswordChangeView.as_view(), name='account_change_password'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('signup/', views.signup, name='signup'),
