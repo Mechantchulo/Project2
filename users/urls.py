149ea1eab4ec153e71e4227228537336468546a6
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-12 19:01:04 +0300

    Streamline URLs for signup, login, and password change views in users app

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from .views import CustomPasswordChangeView
 from django.contrib.auth import views as auth_views
-from . import views
+from .views import signup, CustomPasswordChangeView, login_view, logout_view
 
 urlpatterns = [
+    path('signup/', signup, name='signup'),
     path('password/change/', CustomPasswordChangeView.as_view(), name='account_change_password'),
-    path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('login/', login_view, name='login'),  # Using custom login view for additional control
+    path('logout/', logout_view, name='logout'),  # Using custom logout view for additional control
 ]
+
+
+# urlpatterns = [
+#     path('password/change/', CustomPasswordChangeView.as_view(), name='account_change_password'),
+#     path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
+#     path('signup/', views.signup, name='signup'),
+#     path('logout/', auth_views.LogoutView.as_view(), name='logout')
+# ]
