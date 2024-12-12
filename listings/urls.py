6756c59c22c28dc800af4366eac03753af5f252e
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-13 01:40:04 +0300

    Update URLs to include filtering by property status

--- listings/urls.py
+++ listings/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import listing_create, listing_list, listing_update, listing_delete, listing_detail
+from .views import listing_create, listing_list, listing_update, listing_delete, listing_detail, listing_filter
 from . import views
 
 app_name = 'listings' # Define the namespace
@@ -10,5 +10,6 @@ urlpatterns = [
     path('<int:pk>/update/', listing_update, name='listing_update'),
     path('<int:pk>/delete/', listing_delete, name='listing_delete'),
     path('detail/<int:pk>/', listing_detail, name='listing_detail'),
-path('search/', views.property_search, name='property_search'),
+path('filter/<str:status>/', listing_filter, name='listing_filter'
+    path('search/', views.property_search, name='property_search'),
 ]
