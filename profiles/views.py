5d5db8272e907308b62aa622702e60b64226e228
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-12 10:55:34 +0300

    Create profile view to manage user profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ProfileForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=request.user.profile)
+    return render(request, 'profiles/profile.html', {'form': form})
+
