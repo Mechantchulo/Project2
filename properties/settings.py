9cab32bcd5e39f3bf177dcfe51f2e1e89720450d
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-12 18:05:05 +0300

    Add custom template tags directory to Django settings

--- properties/settings.py
+++ properties/settings.py
@@ -128,6 +128,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'form_tags': 'templatetags.form_tags',
+            }
         },
     },
 ]
