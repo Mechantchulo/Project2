78695717391998499baab8b9712326d323bd2c35
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-16 09:09:24 +0300

    Imported Templateview

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from .views import contact_view
 
 urlpatterns = [
     path('contact/', contact_view, name='contact'),  # URL for the contact form
-    # path('success/', TemplateView.as_view(template_name='success.html'), name='success'),  # Success page
+    path('success/', TemplateView.as_view(template_name='success.html'), name='success'),  # Success page
 ]
