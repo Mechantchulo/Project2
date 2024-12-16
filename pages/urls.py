7b910f4b4a56215cb4cbc52d822243b1355bcda8
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-16 15:02:02 +0300

    Make changes to about.html, pages/urls.py and, about and contact css files

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views import contact_view
+from .views import contact_view, about_view
 
 urlpatterns = [
     path('contact/', contact_view, name='contact'),  # URL for the contact form
     path('success/', TemplateView.as_view(template_name='success.html'), name='success'),  # Success page
-        path('about/', about_view, name='about'),  # URL for the about page
+    path('about/', about_view, name='about'),  # URL for the about page
 
 ]
