6ef9a90cbe0d4c5c89ec47b109aa2a7b8ee56ede
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-18 10:46:34 +0300

    Create Booking model

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    service = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.service} on {self.date} at {self.time}"
